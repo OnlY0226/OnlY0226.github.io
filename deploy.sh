1216ebdeef0d5d7c22dc27e364809cbc8e9d0fc9
Oy <OY.Ou@weltronics.com>
2023-08-14 13:26:52 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -9,5 +9,5 @@ git commit -m 'deploy'
 # 如果你要部署在 https://<USERNAME>.github.io/<REPO>
 # git push -f https://github.com/OnlY0226/oy.github.io.git main:gh-pages
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:OnlY0226/oy.github.io.git master
+git push -f git@github.com:OnlY0226/oy.github.io.git main
 cd -
