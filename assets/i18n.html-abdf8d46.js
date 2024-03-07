0d91123bf4a1ad37d62a5d16d112242d903c8897
OnlY0226 <55518632+OnlY0226@users.noreply.github.com>
2024-03-07 02:02:40 +0000

    Deploying to gh-pages from @ OnlY0226/OnlY0226.github.io@03ce5241ecb8bdb1512bd55f68fce85345b6839d 🚀

--- assets/i18n.html-abdf8d46.js
+++
@@ -1,8 +0,0 @@
-import{_ as e}from"./plugin-vue_export-helper-c27b6911.js";import{o as n,c as a,f as i}from"./app-bc17a636.js";const s={},r=i(`<h1 id="i18n" tabindex="-1"><a class="header-anchor" href="#i18n" aria-hidden="true">#</a> i18n</h1><h2 id="关闭警告" tabindex="-1"><a class="header-anchor" href="#关闭警告" aria-hidden="true">#</a> 关闭警告</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>    vueI18n: {
-        silentTranslationWarn: true,
-        missingWarn: false,
-        silentFallbackWarn: true,
-        fallbackWarn: false,
-        ...
-    }
-</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,3),d=[r];function l(t,c){return n(),a("div",null,d)}const v=e(s,[["render",l],["__file","i18n.html.vue"]]);export{v as default};
