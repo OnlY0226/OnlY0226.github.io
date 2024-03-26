0cff84b6aee76198960d87cc1f14addda417015a
OnlY0226 <55518632+OnlY0226@users.noreply.github.com>
2024-03-26 02:26:18 +0000

    Deploying to gh-pages from @ OnlY0226/OnlY0226.github.io@046dcd16e3d59ba84f57b95f171f3d3c7d31d952 🚀

---
+++ assets/js.html-00c248eb.js
@@ -0,0 +1,30 @@
+import{_ as e}from"./plugin-vue_export-helper-c27b6911.js";import{o as i,c as n,f as a}from"./app-aa7ebc63.js";const r={},d=a(`<h1 id="js" tabindex="-1"><a class="header-anchor" href="#js" aria-hidden="true">#</a> js</h1><h2 id="数组" tabindex="-1"><a class="header-anchor" href="#数组" aria-hidden="true">#</a> 数组</h2><h3 id="数组变树" tabindex="-1"><a class="header-anchor" href="#数组变树" aria-hidden="true">#</a> 数组变树</h3><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>toTree (data) {
+  // 空数组
+  const result = []
+  if (!Array.isArray(data)) return result // 判断不是数组  直接返回
+  data.forEach(item =&gt; {
+    delete item.children
+  }) // 清空children
+  const map = {}
+  data.forEach(item =&gt; { map[item.id] = JSON.parse(JSON.stringify(item)) })
+  data.forEach(item =&gt; {
+    const parent = map[item.parentId]
+    if (parent) {
+      if (!parent.children) parent.children = []
+      parent.children.push(item)
+    } else {
+      result.push(item)
+    }
+  })
+  return result
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="过滤树级数据" tabindex="-1"><a class="header-anchor" href="#过滤树级数据" aria-hidden="true">#</a> 过滤树级数据</h3><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>filter (data) {
+  data.map(item =&gt; item.children &amp;&amp; (item.children = this.filter(item.children)))
+  var newData = data.filter(item =&gt; {
+    if (!item.reviewProposalCount) item.reviewProposalCount = 0
+    if (!item.expertCount) item.expertCount = 0
+    return Number(item.reviewProposalCount) &gt; Number(item.expertCount * 25) || (item.children &amp;&amp; item.children.length !== 0)
+  })
+  return newData
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="浏览器对象" tabindex="-1"><a class="header-anchor" href="#浏览器对象" aria-hidden="true">#</a> 浏览器对象</h2><h3 id="blob" tabindex="-1"><a class="header-anchor" href="#blob" aria-hidden="true">#</a> blob</h3><h3 id="上传空文件" tabindex="-1"><a class="header-anchor" href="#上传空文件" aria-hidden="true">#</a> 上传空文件</h3>`,9),t=[d];function s(l,c){return i(),n("div",null,t)}const u=e(r,[["render",s],["__file","js.html.vue"]]);export{u as default};
