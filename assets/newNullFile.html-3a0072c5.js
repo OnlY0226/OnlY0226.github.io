825003ab988b7efe0d30db1e0804a8d36b4e4c57
OnlY0226 <55518632+OnlY0226@users.noreply.github.com>
2024-05-22 02:39:04 +0000

    Deploying to gh-pages from @ OnlY0226/OnlY0226.github.io@796092f6239c48c5037cfa74828d32efee79eeb2 🚀

---
+++ assets/newNullFile.html-3a0072c5.js
@@ -0,0 +1,13 @@
+import{_ as e}from"./plugin-vue_export-helper-c27b6911.js";import{o as i,c as n,g as l}from"./app-4371a464.js";const t={},a=l(`<h1 id="空文件创建" tabindex="-1"><a class="header-anchor" href="#空文件创建" aria-hidden="true">#</a> 空文件创建</h1><ol><li>在public新建文件<br> /public/docTemplate/template.xlsx<br> /public/docTemplate/template.docx<br> /public/docTemplate/template.pptx<br> /public/docTemplate/template.pdf</li></ol><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>const ExtensionMimeTypeMap = {
+    &#39;pptx&#39;: &#39;application/vnd.openxmlformats-officedocument.presentationml.presentation&#39;,
+    &#39;docx&#39;: &#39;application/vnd.openxmlformats-officedocument.wordprocessingml.document&#39;,
+    &#39;xlsx&#39;: &#39;application/vnd.openxmlformats-officedocument.spreadsheetml.sheet&#39;,
+    &#39;pdf&#39;: &#39;application/pdf&#39;
+}
+async function createFile(fileType: &#39;docx&#39; | &#39;xlsx&#39; | &#39;pptx&#39; | &#39;pdf&#39;, name: string) {
+    const path = \`/docTemplate/template.\${fileType}}\`
+    const file = await fetch(path)
+    const fileArrayBuffer = await file.arrayBuffer()
+    return new File([fileArrayBuffer], \`\${name}.\${fileType}\`,{type: ExtensionMimeTypeMap[fileType]})
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,3),c=[a];function s(d,r){return i(),n("div",null,c)}const m=e(t,[["render",s],["__file","newNullFile.html.vue"]]);export{m as default};
